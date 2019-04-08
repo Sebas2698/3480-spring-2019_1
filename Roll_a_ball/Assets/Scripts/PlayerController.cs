404768b83b14ae8a05650ef370b112241bece8fc
Sebastian Rivera <Sebitasrivera2698@gmail.com>
2019-04-08 17:18:12 -0400

    dkj

---
+++ Roll_a_ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SocialPlatforms.Impl;
+using System;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+    public Text life;
+    public Text lose;
+
+    private Rigidbody rb;
+    private int count;
+    private int Life;
+  
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        Life = 3;
+        SetCountText();
+        setlife();
+        winText.text = "";
+        lose.text = "";
+      
+    }
+
+    private void FixedUpdate () 
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce (movement * speed);
+
+        if (Input.GetKey("escape"))
+            Application.Quit();
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+       if (other.gameObject.CompareTag("Pick up"))
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            SetCountText();
+            setlife();
+
+        }
+       else if (other.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("I hit an enemy");
+            Life--;
+            setlife();
+            other.gameObject.SetActive(false);
+        }
+       if (count == 16)
+        {
+            transform.position = new Vector3(18.63f, 13.75f, -128.5f);
+        }
+    }
+
+ 
+
+    void SetCountText ()
+    {
+        countText.text = "Count:" + count.ToString();
+        if (count >= 33)
+        {
+            winText.text = "CONGRATS YOU WIN!";
+            Time.timeScale = 0f;
+        }
+    }
+
+    void setlife ()
+    {
+        life.text = "Lives:" + Life.ToString();
+        if(Life <= 0)
+        {
+            lose.text = "You're a LOSER!!";
+            Time.timeScale = 0f;
+
+        }
+    }
+
+}
+
+
---
+++ Tutorial 2.5/Assets/Coding/Playercontroller.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Playercontroller : MonoBehaviour
+{
+
+    private Rigidbody2D rb2d;
+    private int count;
+
+    public float speed;
+    public float jumpforce;
+    public Text countText;
+    public Text winText;
+
+    
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        winText.text = "";
+        SetCountText();
+          
+    }
+     void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector2 movement = new Vector2(moveHorizontal, 0);
+
+        rb2d.AddForce(movement * speed);
+
+        if (Input.GetKey("escape"))
+            Application.Quit();
+    }
+
+
+     void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag ("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                rb2d.AddForce(new Vector2(0, jumpforce), ForceMode2D.Impulse);
+            }
+
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString ();
+        if (count >= 4)
+        {
+            winText.text = "You Win";
+        }
+    }
+
+}
